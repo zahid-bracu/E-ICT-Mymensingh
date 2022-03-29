e52d31d207b3aa07492ff94177c6f81ae3123aa1
zahid-bracu <zahid.mym@gmail.com>
2022-03-29 14:29:33 +0600

    ok

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggle_contact() {
+    var x = document.getElementById("contact-box");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
